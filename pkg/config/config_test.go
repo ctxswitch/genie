28561cc856c5df22b68fab09c940810d2f42e002
Rob Lyon <rob@ctxswitch.com>
2023-07-05 14:23:26 -0700

    fix config tmpl conversion and checks

---
+++ pkg/config/config_test.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadAll(t *testing.T) {
+	path, err := os.Getwd()
+	require.NoError(t, err)
+
+	dir := fmt.Sprintf("%s/../../genie.d", path)
+	c, err := LoadAll(dir)
+	assert.NoError(t, err)
+
+	fmt.Printf("%v\n", c.Events)
+}
